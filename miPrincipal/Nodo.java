c22d8fc35565b69c069d9b2adaabbc9b7acedc51
AntonioMartinezJuarez <158599405+AntonioMartinezJuarez@users.noreply.github.com>
2024-03-12 16:49:35 +0000

    nuevo

--- miPrincipal/Nodo.java
+++ miPrincipal/Nodo.java
@@ -1,4 +1,29 @@
 package miPrincipal;
 public class Nodo<T>{
-    
+    //Atributo valor de tipo T almacena la referencia al objeto 
+    //que se guarda en el nodo
+    private t valor;
+    //Referencia al siguiente nodo enlazado
+    private Nodo<t> siguiente;
+    //constructor por defecto
+    public Nodo(){
+        valor = null;
+        siguiente = null;
+    }
+    //devuelve el valor
+    public t getValor(){
+        return valor;
+    }
+    //modifica el valor
+    public void setValor(t valor){
+        this.valor = valor;
+    }
+    //Devuelve el atributo siguiente
+    public Nodo<t> getSisguiente(){
+        return siguiente;
+    }
+    //modifica el atributo siguiente
+    public void setSiguiente(Nodo <t> siguiente){
+        this.siguiente = siguiente;
+    }
 }
---
+++ miPrincipal/Pila.java
@@ -0,0 +1,45 @@
+package miPrincipal;
+public class Pila <T> {
+    //el atributo cabeza apunta al tope de la pila
+    private Nodo<T>cabeza;
+    //almacena el total de elementos de la pila
+    private int tamanio;
+    //constructor por defecto
+    public Pila(){
+        cabeza = null;
+        tamanio = 0;
+    }
+    //devuelve el total de elementos de la pila
+    public int getTamanio(){
+        return tamanio;
+    }
+    //verifica si la pila esta vacia
+    public boolean esVacia(){
+        return(cabeza == null);
+    }
+    public void apilar(T valor){
+        //crear un nuevo Nodo
+        Nodo<T> nuevo = new Nodo<T>();
+        //fijar el valor dentro del nodo
+        nuevo.setValor(valor);
+        if (esVacia()) {
+            //la cabeza apunta al nuevo nodo
+            cabeza = nuevo;
+        }else{
+            //se enlaza el campo siguiente de nuevo con la cabeza
+            nuevo.setSiguiente(cabeza);
+            //la nueva cabeza de la pila pasa a ser nuevo
+            cabeza = nuevo;
+        }
+        //incrementar el tamaño por que hay un nuevo elemento
+        tamanio++;
+    }
+    //eliminar un elemento de la pila
+    public void retirar(){
+        if (!esVacia()) {
+            cabeza = cabeza.getSisguiente();
+            tamanio--;
+        }
+    }
+    
+}
