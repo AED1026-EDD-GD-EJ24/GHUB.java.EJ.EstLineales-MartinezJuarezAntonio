62568a49ac1eedb471300a825902fb2575122bf1
AntonioMartinezJuarez <158599405+AntonioMartinezJuarez@users.noreply.github.com>
2024-04-18 16:14:57 +0000

    Actualizacion

--- miPrincipal/DemoLinkedListCuadrupedos.java
+++ miPrincipal/DemoLinkedListCuadrupedos.java
@@ -13,17 +13,43 @@ public class DemoLinkedListCuadrupedos {
         System.out.println("ArrayList de cuadrupedos");
         System.out.println("************************");
         System.out.println();
-        try {
-            List<Cuadrupedo>cuadrupedos = new LinkedList<>() {
-                String[] tipos = {"Leon","Perro","Gato","Elefante"};
-                Random r = new Random();
-                for(int i = 1; i <= 10000;i++){
-                    
-                }  
-            };
-        } catch (Exception e) {
+        try{
+            List<Cuadrupedo> cuadrupedos = new LinkedList<>();
+            String [] tipos ={"León","Gato","Perro","Elefante"};
+            Random random = new Random();
+            for(int i=1;i<=10000;i++){
+                String tipo = tipos[random.nextInt(tipos.length)];
+                cuadrupedos.add(new Cuadrupedo(i, tipo));
+            }
+            int leonCount = 0, gatoCount =0, perroCount=0,elefanteCount=0;
+            for(Cuadrupedo cuadrupedo:cuadrupedos){
+                if(cuadrupedo.getIDcuadrupedo()%1000==0){
+                    System.out.println("ID"+cuadrupedo.getIDcuadrupedo()+
+                      ",tipo: "+cuadrupedo.getTipo());
+                }
+                switch (cuadrupedo.getTipo()) {
+                    case "León":
+                        leonCount++;
+                        break;
+                    case "Gato":
+                        gatoCount++;
+                        break;
+                    case "Perro":
+                        perroCount++;
+                        break;
+                    case "Elefante":
+                       elefanteCount++;
+                       break;
+                }
+
+            }
+            System.out.println("Resumen");
+            System.out.println("Leones: "+leonCount);
+            System.out.println("Elefantes: "+elefanteCount);
+            System.out.println("Perros: "+perroCount);
+            System.out.println("Gatos: "+gatoCount);
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
 }
-   
